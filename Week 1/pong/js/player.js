d029e89e2dce80df8478d64b1783ca3b8bf0247f
KayakCat <lmhall@email.neit.edu>
2024-07-24 08:53:50 -0400

    player class pong

---
+++ Week 1/pong/js/player.js
@@ -0,0 +1,11 @@
+class Player {
+    constructor(name, score = 0, highScore = 0, pad) {
+        this.name = name;
+        this.score = score;
+        this.highScore = highScore;
+        this.pad = pad;
+    }
+}
+    
+    
+    
